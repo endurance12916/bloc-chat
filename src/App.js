461fc025fc4bfc7c9201207e74f9aa8230e8ae4c
Zixing(Luke) Li <zl82@cornell.edu>
2017-07-20 22:48:06 -0400

    add firebase rooms and messages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import * as firebase from 'firebase';
 import { Grid, Row, Col, Button } from 'react-bootstrap'
 
-const config = {  
+const config = {
     apiKey: "AIzaSyCXeADl350Vv4FALlgr4O4VtWztXWJFw3g",
     authDomain: "bloc-chat-4d62e.firebaseapp.com",
     databaseURL: "https://bloc-chat-4d62e.firebaseio.com",
@@ -17,27 +17,81 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      roomNum: 1
+      rooms: [],
+      room: 'Room',
+      messages: [],
+      message: '',
     }
   }
 
   componentDidMount() {
-    const rootRef = firebase.database.ref();
-    const roomRef = rootRef.child("rooms");
+    const rootRef = firebase.database().ref().child('rooms');
+    const roomRef = rootRef.child("room1");
+    roomRef.on('value', (snapshot) => {
+    // firebase.database().ref('rooms/').on('value', (snapshot) => {
+      const currentRooms = snapshot.val();
+        if (currentRooms != null) {
+          this.setState({
+            rooms: currentRooms
+          });
+      }
+    })
+
     // the on() method allows syncing data in real time. To use it, attach it to a Ref -> everytime data changes at the location of rooms, it provides this callback function that returns a new set of data
-    roomRef.on('value', snap => {
-      this.setState({
-        roomNum: snap.val()
-      });
+    firebase.database().ref('messages/').on('value', (snapshot) => {
+      const currentMessages = snapshot.val();
+      if (currentMessages != null) {
+        this.setState({
+          messages: currentMessages
+        });
+      }
     });
   }
 
+  addRoom = (event) => {
+    const nextRoom = {
+      id: this.state.rooms.length,
+      text: this.state.room
+    }
+
+    firebase.database().ref('rooms/'+nextRoom.id).set(nextRoom)
+  }
+
+  updateMessage = (event) => {
+    console.log('updateMessage: ' + event.target.value)
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  submitMessage = (event) => {
+    console.log('submitMessage ' + this.state.message)
+    const nextMessage = {
+      id: this.state.messages.length,
+      text: this.state.message
+    }
+
+    firebase.database().ref('messages/'+nextMessage.id).set(nextMessage)
+  }
+
   render() {
+    const currentMessage = this.state.messages.map((message, i) => {
+      return (
+        <li key={message.id}>{message.text}</li>
+      )
+    })
+
+    const currentRoom = this.state.rooms.map((room, i) => {
+      return (
+        <li key={room.id}>{room.text}</li>
+      )
+    })
+
     return (
       <div className="App">
         <Grid fluid>
           {/* Title */}
-          <Row className="show-grid title row text-center">
+          <Row className="title row text-center">
             <Col sm={12}>
               <h1>Bloc Chat App</h1>
             </Col>
@@ -48,20 +102,17 @@ class App extends Component {
             <Col sm={3} xsHidden className="room-section">
               <Col sm={11} smOffset={1}>
                 <h2>Bloc Chat</h2>
-                <ul className="list-unstyled">
-                  <li>Room1</li>
-                  <li>Room2</li>
-                  <li>Room3</li>
-                </ul> 
-                <Button className="pull-left">New Room</Button>
+                <ul className="list-unstyled">{currentRoom}</ul> 
+                <Button onClick={this.addRoom}>New Room</Button>
               </Col>
             </Col> 
             {/* Message section */}
             <Col sm={9} className="message-section">
               <Col sm={11} smOffset={1}>
-                <h2>Room {this.state.roomNum}</h2>
-                <p className="lead">messages</p>
-                <i className="glyphicon glyphicon-send"></i>
+                <h2>{this.state.room}</h2>
+                <ul className="list-unstyled">{currentMessage}</ul>
+                <input onChange={this.updateMessage} type="text" placeholder="Message" />
+                <Button onClick={this.submitMessage}><i className="glyphicon glyphicon-send"></i></Button>
               </Col>
             </Col>
           </Row>
